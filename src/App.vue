13a875683d4c748305bea1bca2d17af71149f586
文智 <802607+zew13@users.noreply.github.com>
2018-09-15 09:39:30 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+#app
+  reply(placeholder="请输入 … ")
+</template>
+
+<script>
+import reply from "./components/reply";
+
+export default {
+  name: "App",
+  components: {
+    reply
+  }
+};
+</script>
+
+<style>
+textarea{
+  font-size:16px;
+  padding:16px;
+  box-sizing:border-box;
+}
+</style>
---
+++ src/components/reply.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+  textarea(:placeholder="placeholder")
+</template>
+
+<script>
+export default {
+  name: "reply",
+  props: ["placeholder"],
+  data() {
+    return {};
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+textarea {
+  resize: none;
+  outline: 0;
+  width: 100%;
+  height: 80px;
+}
+</style>
